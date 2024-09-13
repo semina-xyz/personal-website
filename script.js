484e759450420017579c3ecc43713690e3725c10
semina-xyz <seminapek97@gmail.com>
2024-09-13 09:45:36 +0200

    moved files

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function cv() {
         <button>Education</button>
         <button>Skills</button>
         <div id="stuff"></div>
-        <button> <a href="cv.pdf" class="download-btn" download="filename.pdf">Download Resume</a>
+        <button> <a href="files/cv.pdf" class="download-btn" download="files/cv.pdf">Download Resume</a>
 </body></button>`
 
         
@@ -57,7 +57,7 @@ function about() {
         <p>
         
             In my internships and projects, I’ve developed a strong foundation in programming languages like JavaScript, HTML, CSS, and SQL, along with practical experience in IT processes. I'm always eager to tackle new challenges and contribute to impactful projects that make a difference.</p></div>
-                <img src="headshot1.png" alt="headshot">
+                <img src="files/headshot1.png" alt="headshot">
     </div></div>`;
     
     count++;
