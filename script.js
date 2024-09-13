980ea0fd645633abc28ac08af4e95fb80137a41c
semina-xyz <seminapek97@gmail.com>
2024-09-13 15:18:19 +0200

    renamed headshot

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function about() {
         <p>
         
             In my internships and projects, I’ve developed a strong foundation in programming languages like JavaScript, HTML, CSS, and SQL, along with practical experience in IT processes. I'm always eager to tackle new challenges and contribute to impactful projects that make a difference.</p></div>
-                <img src="headshot1.png" alt="headshot">
+                <img src="headshot.png" alt="headshot">
     </div></div>`;
     
     count++;
